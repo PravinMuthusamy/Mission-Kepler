c3a4234f21b14f40810a4b45c985d80e21d6d70b
Pravin <pravin.muthusamy@cdw.com>
2023-12-11 09:51:43 +0530

    Style changes done

--- sitback/src/components/Header/index.jsx
+++ sitback/src/components/Header/index.jsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Header.module.css'; 
 import { FaSortDown } from 'react-icons/fa';
-import * as constants from '../../constants/AppConstants'
+import * as constants from '../../constants/AppConstants';
 
-const Header = ({categories}) => {
+const Header = ({ categories }) => {
+  const [activeCategory, setActiveCategory] = useState(null);
+
+  useEffect(() => {
+    const storedCategory = localStorage.getItem('activeCategory');
+    if (storedCategory) {
+      setActiveCategory(storedCategory);
+    }
+  }, []);
+
+  const handleCategoryClick = (category) => {
+    setActiveCategory(category);
+    localStorage.setItem('activeCategory', category);
+  };
+
   return (
     <header className={styles.header}>
       <div className={styles.leftSection}>
-        <a href={`/`} className={styles.sitback}>{constants.SITBACK}</a>
+        <a href={`/`} className={styles.sitback}>
+          {constants.SITBACK}
+        </a>
       </div>
       <nav className={styles.navBar}>
         {categories.map((item) => (
-          <a key={item.category} href={`/categories/${item.category.toLowerCase()}`}>
+          <a
+            key={item.category}
+            href={`/categories/${item.category.toLowerCase()}`}
+            onClick={() => handleCategoryClick(item.category)}
+            className={activeCategory === item.category ? styles.activeCategory : styles.normalCategory}
+          >
             {item.category.toUpperCase()}
           </a>
         ))}
       </nav>
-        <p className={styles.user}>
-          {constants.USER} <FaSortDown className={styles.downIcon} />
-        </p>
+      <p className={styles.user}>
+        {constants.USER} <FaSortDown className={styles.downIcon} />
+      </p>
     </header>
   );
 };
